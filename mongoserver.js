2b1aa292dabea369a9c70fac2219000adb56f4f6
lazikos94 <lazikos94@gmail.com>
2019-10-06 02:04:17 +0300

    added update and show db

--- mongoserver.js
+++ mongoserver.js
@@ -4,6 +4,7 @@ const mongo = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 const assert = require('assert');
+const objectId = require('mongodb').ObjectID;
 
 const app = express(); 
 const port = process.env.PORT || 5000;
@@ -31,8 +32,7 @@ var Schema = mongoose.Schema;
 var dataSchema = new Schema({
     first_name: String,
     last_name: String,
-    address: String,
-    country:String,
+    age: Number,
 },{collection:'data'});
 
 var Data = mongoose.model('data',dataSchema);
@@ -49,16 +49,67 @@ const findDocuments = function(db, callback) {
     });
 }
 
+const deleteDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('data');
+    // Find some documents
+    collection.deleteMany({});
+}
+
 app.post('/mongodb',(req,res)=>{
     const userdata = new Data(req.body);
     userdata.save();
 })
+
 app.get('/mongodb',(req,res)=>{
-    MongoClient.connect(url_mongo,(err,client)=>{
+    MongoClient.connect(url_mongo,{useNewUrlParser: true,useUnifiedTopology:true},(err,client)=>{
         assert.equal(null,err);
-        const db = client.db(mongodb);  
-        findDocuments(db,() =>{
+        const collection = client.db(mongodb);  
+        findDocuments(collection,() =>{
         client.close();
         });
     });         
 })
+
+app.get('/mongoget',(req,res)=>{
+    var  resultArray  = [];
+    MongoClient.connect(url_mongo,{useNewUrlParser: true,useUnifiedTopology:true},(err,client)=>{
+        assert.equal(null, err);
+        var db = client.db(mongodb);
+        var cursor = db.collection('data').find();
+        cursor.forEach((doc, err)=>{ 
+          assert.equal(null, err);
+          resultArray.push(doc);
+        },()=> {
+          client.close();
+          res.json(resultArray);
+        });
+    });
+});        
+
+app.get('/mongodelete',(req,res)=>{
+    MongoClient.connect(url_mongo,{useNewUrlParser: true,useUnifiedTopology:true},(err,client)=>{
+        assert.equal(null,err);
+        const db = client.db(mongodb);  
+        deleteDocuments(db,() =>{
+        client.close();
+        });
+    });   
+});
+app.post('/mongoupdate',(req,res)=>{
+    const data = {
+        'first_name': req.body.first_name,
+        'last_name': req.body.last_name,
+        'age': req.body.age,
+    }
+    const id = req.body._id;
+    MongoClient.connect(url_mongo,{useNewUrlParser: true,useUnifiedTopology:true},(err,client)=>{
+        assert.equal(null,err);
+        const db = client.db(mongodb);
+        db.collection('data').updateOne({"_id":objectId(id)},{$set:data},(err,result)=>{
+            assert.equal(null,err);
+            console.log('Item updated');
+            client.close();
+        });  
+    });   
+});
--- public/files/inputscript.js
+++ public/files/inputscript.js
@@ -1,16 +1,16 @@
 let firstname = document.getElementById('firstname');
 let lastname = document.getElementById('lastname');
-let address= document.getElementById('address');
-let country = document.getElementById('country');
-let date = document.getElementById('date');
-mongodb();
+let age= document.getElementById('age');
+let ufirstname = document.getElementById('ufirstname');
+let ulastname = document.getElementById('ulastname');
+let uage= document.getElementById('uage');
+let id= document.getElementById('id');
 try{
     document.getElementById('submit').addEventListener('click', async ()=>{
         const data = {
             'first_name': firstname.value,
             'last_name': lastname.value,
-            'address': address.value,
-            'country': country.value,
+            'age': age.value,
         }
         const options = {
             method: 'POST',
@@ -28,8 +28,68 @@ catch(error){
     console.log(error);
 }
 
+try{
+    document.getElementById('empty').addEventListener('click', async ()=>{
+        const response = await fetch('/mongodelete');
+        const json = await response.json();
+        console.log(json);
+    });
+}
+catch(error){
+    console.log(error);
+}
+
 async function mongodb(){
-    const response = await fetch('/mongodb');
-    const data = await response.json();
-    console.log(data);
+    try{
+        const response = await fetch('/mongodb');
+        const data = await response.json();
+        console.log(data);
+    }
+    catch(error){
+        console.log("failed to fetch cause empty db",error)
+    }
+}
+
+try{
+    document.getElementById('show').addEventListener('click', async ()=>{
+        const response = await fetch('/mongoget');
+        const json = await response.json();
+        json.forEach((item)=>{
+            $("div.database").append(
+                `<div class='db_content'>
+                    <p id='data'> _id: ${item._id}</p>
+                    <p id='data'> First Name: ${item.first_name}</p>
+                    <p id='data'> Last Name: ${item.last_name}</p>
+                    <p id='data'> Age: ${item.age}</p>
+                </div>`
+            );
+        })       
+        console.log(json);
+    });
+}
+catch(error){
+    console.log(error);
+}
+try{
+    document.getElementById('update_info').addEventListener('click', async ()=>{
+        const data = {
+            'first_name': ufirstname.value,
+            'last_name': ulastname.value,
+            'age': uage.value,
+            '_id': id.value,
+        }
+        const options = {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        };
+        const response = await fetch('/mongoupdate', options);
+        const json = await response.json();
+        console.log(json);
+    });
+}
+catch(error){
+    console.log(error);
 }
